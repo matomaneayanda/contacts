615f5d4baa056f6e6ef6587a9aeed6aff514c68f
Mpendulo Cele <celempe@amazon.com>
2024-02-18 15:14:21 +0200

    Fixing a bug in contact contacts_db.sql

--- db/contacts_db.sql
+++ db/contacts_db.sql
@@ -2,10 +2,10 @@ create database if not exists contacts;
 
 use contacts;
 
-create table contacts (
-Name varchar(100),
-Last_Name varchar(100),
-Cellphone_number varchar(10)
-primary key (Cellphone_number)
+create table numbers (
+name varchar(100),
+last_name varchar(100),
+cellphone_number varchar(10),
+primary key (cellphone_number)
 );
 commit;
