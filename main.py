5d107895af99b768bf5fc28e098231a6b3831eff
Mpendulo Cele <celempe@amazon.com>
2024-02-18 15:02:33 +0200

    Adding mechanism to execute .sql file and docker-compose.

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import mysql.connector
 
 # Establish a connection to the MySQL server
 conn = mysql.connector.connect(
-    host="your_host",
-    user="your_user",
-    password="your_password",
-    database="your_database"
+    host="127.0.0.1",
+    user="contacts_server",
+    password="password",
 )
 
 # Create a cursor to execute queries
 cursor = conn.cursor()
 
 # Open and read the SQL file
-with open('your_sql_file.sql', 'r') as file:
+with open('db/contacts_db.sql', 'r') as file:
     sql_queries = file.read()
 
 # Split the SQL file content into individual queries
